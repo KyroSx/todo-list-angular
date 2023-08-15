138bf4571f5f81c0b24b7bb40bd9539a5bc4c046
kyroSx <leolub150@gmail.com>
2023-08-15 17:15:04 -0300

    feat: ensure it can filter by todo status

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -4,12 +4,20 @@ import { Todo } from '../models';
 import { TodoBlank } from '../errors';
 import { AddTodoFormService } from '../services/add-todo-form.service';
 
+enum Filter {
+  ALL = 'ALL',
+  COMPLETED = 'COMPLETED',
+  UNCOMPLETED = 'UNCOMPLETED',
+}
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent {
+  filter: Filter = Filter.ALL;
+
   constructor(
     public todos: TodosService,
     public form: AddTodoFormService
@@ -24,13 +32,33 @@ export class TodoComponent {
         this.form.setErrorMessage(error.message);
       }
     }
+
+    this.viewAll();
   }
 
   toggleTodo(todo: Todo) {
     this.todos.toggleTodo(todo);
+    this.viewAll();
   }
 
   removeTodo(todo: Todo) {
     this.todos.removeTodo(todo);
+    this.viewAll();
+  }
+
+  viewAll() {
+    this.applyFilter(Filter.ALL);
+    this.filter = Filter.ALL;
+  }
+
+  applyFilter(filter: Filter) {
+    switch (filter) {
+      case Filter.ALL:
+        return this.todos.resetFilter();
+      case Filter.COMPLETED:
+        return this.todos.filterByCompleted();
+      case Filter.UNCOMPLETED:
+        this.todos.filterByUncompleted();
+    }
   }
 }
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { TodoRoutingModule } from './todo-routing.module';
 import { TodoComponent } from './todo.component';
 import { SharedComponentsModule } from '../components';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [TodoComponent],
-  imports: [CommonModule, TodoRoutingModule, SharedComponentsModule],
+  imports: [
+    CommonModule,
+    TodoRoutingModule,
+    SharedComponentsModule,
+    MatButtonToggleModule,
+  ],
 })
 export class TodoModule {}
